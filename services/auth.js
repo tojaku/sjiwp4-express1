c559a6f6d950646ba6fca5cee53eb95223be8c92
Tomislav J <tojaku@gmail.com>
2024-01-25 11:15:43 +0100

    users data change

--- services/auth.js
+++ services/auth.js
@@ -1,6 +1,7 @@
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const jwt = require("jsonwebtoken");
+const { db } = require("./db.js");
 
 function getUserJwt(id, email, name, role, expDays = 7) {
     const tokenData = {
@@ -25,6 +26,7 @@ function authRequired(req, res, next) {
     if (!req.user) throw new Error("Potrebna je prijava u sustav");
     next();
 }
+
 // MIDDLEWARE FOR PARSING AUTH COOKIE
 function parseAuthCookie(req, res, next) {
     const token = req.cookies[process.env.AUTH_COOKIE_NAME];
@@ -40,8 +42,20 @@ function parseAuthCookie(req, res, next) {
     next();
 }
 
+function checkEmailUnique(email) {
+    const stmt = db.prepare("SELECT count(*) FROM users WHERE email = ?;");
+    const result = stmt.get(email);
+
+    if (result["count(*)"] >= 1) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 module.exports = {
     getUserJwt,
     authRequired,
-    parseAuthCookie
+    parseAuthCookie,
+    checkEmailUnique
 };
